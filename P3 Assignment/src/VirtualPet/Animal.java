d2888c3f1bfd458f849acf563a32fb5f2f391289
KanyeWestt <42123623+KanyeWestt@users.noreply.github.com>
2018-08-15 12:23:36 +1200

    Add files via upload

---
+++ P3 Assignment/src/VirtualPet/Animal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VirtualPet;
+
+/**
+ *
+ * @author gsj6766
+ */
+public class Animal {
+    sad
+}
